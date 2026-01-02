e5a36d3846a21a926016da9d9dfa6b72083f92f5
abhijithwinddaa <abhijithyadav786@gmail.com>
2026-01-02 16:15:29 +0530

    feat: add express server with chat endpoint and necessary dependencies

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,18 @@
 const input = document.querySelector("#input");
 const chatContainer = document.querySelector("#chat-container");
 const askbtn = document.querySelector("#ask");
-console.log("Script loaded");
+
+const threadId =
+  Date.now().toString(36) + Math.random().toString(36).substring(2, 8);
 
 input?.addEventListener("keyup", handleEnter);
 askbtn?.addEventListener("click", handleAsk);
 
-function generateMessage(text, isUser) {
+const loading = document.createElement("div");
+loading.className = "my-6 animate-pulse";
+loading.textContent = "Thinking...";
+
+async function generateMessage(text, isUser) {
   const msg = document.createElement("div");
   if (isUser) {
     msg.className = "my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit";
@@ -15,25 +21,51 @@ function generateMessage(text, isUser) {
   }
   msg.textContent = text;
   chatContainer?.appendChild(msg);
+  input.value = "";
+
+  chatContainer?.appendChild(loading);
+
+  const assistantMessage = await callServer(text);
+
+  const assistantMsgElem = document.createElement("div");
+  assistantMsgElem.className = `max-w-fit`;
+  assistantMsgElem.textContent = assistantMessage;
+  loading.remove();
+  chatContainer?.appendChild(assistantMsgElem);
 }
 
-function handleAsk(e) {
+async function callServer(inputText) {
+  const response = await fetch("http://localhost:3002/chat", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ threadId, message: inputText }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data = await response.json();
+  return data.message;
+}
+
+async function handleAsk(e) {
   const text = input?.value.trim();
   if (!text) {
     return;
   }
-  generateMessage(text, true);
-  input.value = "";
+  await generateMessage(text, true);
 }
 
-function handleEnter(event) {
+async function handleEnter(event) {
   if (event.key === "Enter" && !event.shiftKey) {
     event.preventDefault();
     const text = input?.value.trim();
     if (!text) {
       return;
     }
-    generateMessage(text, true);
-    input.value = "";
+    await generateMessage(text, true);
   }
 }
